f2e981b41227fddb16f46e8390f4b224643e050b
Or <orrodrigez1@gmail.com>
2022-11-13 19:48:46 +0200

    Style the book cards

--- scripts.js
+++ scripts.js
@@ -24,6 +24,7 @@ const bookLibrary = document.getElementById('library');
 myLibrary.forEach((book) => {
   const bookCard = document.createElement('div');
   bookCard.classList.add('book-card');
+
   const bookDescription = document.createElement('div');
   bookDescription.classList.add('book-description');
 
@@ -32,18 +33,36 @@ myLibrary.forEach((book) => {
   title.textContent = book.title;
 
   const author = document.createElement('p');
-  author.classList.add('book-author');
-  author.textContent = `Author: ${book.author}`;
+  const authorName = document.createElement('p');
+  const authorDiv = document.createElement('div');
+  author.classList.add('sub-title');
+  author.textContent = `Author: `;
+  authorName.classList.add('field-content')
+  authorName.textContent = book.author;
+  authorDiv.classList.add('book-field');
+  authorDiv.append(author, authorName);
 
   const pages = document.createElement('p');
-  pages.classList.add('book-pages')
-  pages.textContent = `Pages: ${book.pages}`;
+  const pageCount = document.createElement('p');
+  const pageCountDiv = document.createElement('div');
+  pages.classList.add('sub-title')
+  pages.textContent = `Pages: `;
+  pageCount.classList.add('field-content')
+  pageCount.textContent = book.pages;
+  pageCountDiv.classList.add('book-field');
+  pageCountDiv.append(pages, pageCount);
 
   const read = document.createElement('p');
-  read.classList.add('book-read');
-  read.textContent = `Read: ${book.read}`;
+  const isRead = document.createElement('p');
+  const isReadDiv = document.createElement('div');
+  read.classList.add('sub-title');
+  read.textContent = `Read: `;
+  isRead.classList.add('field-content');
+  isRead.textContent = book.read
+  isReadDiv.classList.add('book-field');
+  isReadDiv.append(read, isRead);
 
-  bookDescription.append(author, pages, read)
+  bookDescription.append(authorDiv, pageCountDiv, isReadDiv);
   bookCard.append(title, bookDescription);
   bookLibrary.appendChild(bookCard);
 })
